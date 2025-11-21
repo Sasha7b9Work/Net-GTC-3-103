0d58cacf4f89713482718ce5b3aa0e04e287d946
Sasha7b9Work <Phuntik-it@tut.by>
2025-11-21 14:53:22 +0300

    *

--- sources/scripts/vs_win/assembly.bat
+++ sources/scripts/vs_win/assembly.bat
@@ -15,13 +15,13 @@ cd ../..
 rmdir generated\GUI /s /q
 cd scripts/vs_win
 @echo on
-cmake ../../VS/CMakeLists.txt -B../../generated/GUI  -G "Visual Studio 17 2022" -A Win32 -DCMAKE_BUILD_TYPE=Release -DBUILD_USE_STATIC_RUNTIME=ON
+cmake ../../VS/CMakeLists.txt -B../../generated/GUI  -G "Visual Studio 18 2026" -A Win32 -DCMAKE_BUILD_TYPE=Release -DBUILD_USE_STATIC_RUNTIME=ON
 @echo off
 
 :BUILDING
 if %isBuild%==0 goto EXIT
 @echo on
-MSBuild.exe ../../generated/GUI/RES-Network-GUI.sln -clp:ErrorsOnly;WarningsOnly -nologo /m
+MSBuild.exe ../../generated/GUI/RES-Network-GUI.slnx -clp:ErrorsOnly;WarningsOnly -nologo /m
 @echo off
 goto EXIT
 
